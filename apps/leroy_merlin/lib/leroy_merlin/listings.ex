c13c876000e2e947330a8e080e26a5bb1b800592
vitaminwater <stantin04@gmail.com>
2017-06-09 10:37:45 +0200

    free session between listing pages

--- apps/leroy_merlin/lib/leroy_merlin/listings.ex
+++ apps/leroy_merlin/lib/leroy_merlin/listings.ex
@@ -12,31 +12,31 @@ defmodule LeroyMerlin.Listings do
   defp loop() do
     Logger.info("Scrapping Leroy Merlin")
 
-    Hound.start_session()
     total = crawl_listing(0)
     Logger.info("LeroyMerlin: Found #{total} pages")
-    Hound.end_session()
 
     sleep()
     loop()
   end
 
   defp crawl_listing(offset, total \\ 0) do
+    Hound.start_session()
     listing = Scrapping.Server.get_next_listing("www.leroymerlin.fr")
     url = listing_url(listing, offset)
     Logger.info("Listing: #{url}")
     navigate_to("https://www.leroymerlin.fr#{url}")
     tc_vars = execute_script("return tc_vars")
-    n_listed = length(tc_vars["list_products"])
-    Logger.info("#{n_listed} products listed");
-    unless n_listed == 0 do
-      Enum.each(tc_vars["list_products"], &Scrapping.Server.found_page("www.leroymerlin.fr", &1["list_product_url_page"]))
-      crawl_listing(offset+@n_per_page, total+n_listed)
-    end
-    total
+    Hound.end_session()
+    send_listing(tc_vars["list_products"], offset, total)
   end
 
-  # Private
+  defp send_listing([], _offset, total), do: total
+  defp send_listing(products, offset, total) do
+    Enum.each(products, &Scrapping.Server.found_page("www.leroymerlin.fr", &1["list_product_url_page"]))
+    crawl_listing(offset+@n_per_page, total+length(products))
+  end
+
+  # Utils
   
   defp listing_url(listing, offset), do: listing.url |> String.replace("[n_offset]", Integer.to_string(offset)) |> String.replace("[n_per_page]", Integer.to_string(@n_per_page))
 
